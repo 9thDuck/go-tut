0aa404bf2cff4c843f907679966408cd94352db8
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-08-17 12:39:03 +0530

    refactored

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -5,25 +5,26 @@ import "fmt"
 func main() {
 	var revenue, expenses, taxRate float64
 
-	fmt.Print(`Enter revenue: `)
-	fmt.Scan(&revenue)
+	getInputData(`Enter revenue: `, &revenue)
 
-	fmt.Print(`Enter expenses: `)
-	fmt.Scan(&expenses)
+	getInputData(`Enter expenses: `, &expenses)
 
-	fmt.Print(`Enter tax rate: `)
-	fmt.Scan(&taxRate)
+	getInputData(`Enter tax rate: `, &taxRate)
 
-	earningsBeforeTax := revenue - expenses
-
-	earningsAfterTax := earningsBeforeTax - (earningsBeforeTax * taxRate / 100)
-
-	ratio := earningsBeforeTax / earningsAfterTax
-
-	// fmt.Println(earningsBeforeTax)
-	// fmt.Println(earningsAfterTax)
-	// fmt.Println(ratio)
+	earningsBeforeTax, earningsAfterTax, ratio := calculateProfitsAndRatio(revenue, expenses, taxRate)
 
 	fmt.Printf("\nOutput\nEarnings before tx: %.2f\nEarnings after tax: %.2f\nRatio: %.2f\n", earningsBeforeTax, earningsAfterTax, ratio)
+}
 
+func getInputData(label string, variable *float64) {
+	fmt.Print(label)
+	fmt.Scan(variable)
+}
+
+func calculateProfitsAndRatio(revenue, expenses, taxRate float64) (earningsBeforeTax float64, earningsAfterTax float64, ratio float64) {
+	earningsBeforeTax = revenue - expenses
+	earningsAfterTax = earningsBeforeTax - (earningsBeforeTax * taxRate / 100)
+	ratio = earningsBeforeTax / earningsAfterTax
+
+	return earningsBeforeTax, earningsAfterTax, ratio
 }
