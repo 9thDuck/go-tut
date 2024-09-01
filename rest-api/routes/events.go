58e88c3aff1b5c166eedaf2d0951d700fd9f72ea
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-09-01 20:01:37 +0530

    seperated routes logic from main.go

---
+++ rest-api/routes/events.go
@@ -0,0 +1,66 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetHome(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"asdf": "asdf"})
+}
+
+func GetEvents(ctx *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		fmt.Println(fmt.Errorf("error: Could not fetch events. Try again later, details:%v", err))
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Successfully fetched all events", "event": *events})
+}
+
+func PostEvent(ctx *gin.Context) {
+	var event models.Event
+
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request data.", "err": err, "time": time.Now()})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+	err = event.Save()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}
+
+func GetSingleEvent(ctx *gin.Context) {
+	eventIdStr := ctx.Param("id")
+	eventId, err := strconv.ParseInt(eventIdStr, 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the event id"})
+		return
+	}
+
+	fmt.Println("event id", eventId)
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("Could not find the event with id: %v", eventId)})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Successfully fetched the event", "event": event})
+}
---
+++ rest-api/routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/", GetHome)
+	server.GET("/events", GetEvents)
+	server.GET("/events/:id", GetSingleEvent)
+	server.POST("/events", PostEvent)
+}
